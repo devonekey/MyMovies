97e790c625f949f5646cc46e4a8a3b21b5c6f0ba
OneKey <devonekey@gmail.com>
2021-09-28 18:31:42 +0900

    Url로 image를 불러오는 코드 확장

--- app/src/main/java/com/onekey/mymovies/presentation/view/MovieViewHolder.kt
+++ app/src/main/java/com/onekey/mymovies/presentation/view/MovieViewHolder.kt
@@ -4,7 +4,6 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.onekey.mymovies.data.Movie
 import com.onekey.mymovies.databinding.ViewMovieBinding
-import com.squareup.picasso.Picasso
 
 class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val binding = ViewMovieBinding.bind(itemView)
@@ -17,10 +16,6 @@ class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         with(binding) {
             item = movie
             titleTextView.isSelected = true
-
-            Picasso.get()
-                .load(movie.image)
-                .into(thumbnailImageView)
         }
     }
 }
